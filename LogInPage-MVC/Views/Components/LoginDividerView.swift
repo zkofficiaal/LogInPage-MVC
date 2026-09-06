e98db1e48bedf4fdba01462febec3c15e61f8d36
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-06 11:39:06 -0700

    LoginDividerView added

--- LogInPage-MVC/Views/Components/LoginDividerView.swift
+++ LogInPage-MVC/Views/Components/LoginDividerView.swift
@@ -5,3 +5,16 @@
 //  Created by Z.K   on 06/09/2026.
 //
 
+import SwiftUI
+
+/// "OR CONTINUE WITH" divider between the primary action and social logins.
+struct LoginDividerView: View {
+    var text: String = "OR CONTINUE WITH"
+
+    var body: some View {
+        Text(text)
+            .font(.system(size: 11, weight: .semibold))
+            .foregroundColor(AppTheme.secondaryText)
+            .tracking(0.5)
+    }
+}
